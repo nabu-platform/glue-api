62a1467d36129ab0537f96142fc19d0501a42555
alex <alex@nabu.be>
2015-01-22 11:35:03 +0100

    Added support for enumerations

--- src/main/java/be/nabu/glue/api/ParameterDescription.java
+++ src/main/java/be/nabu/glue/api/ParameterDescription.java
@@ -1,7 +1,10 @@
 package be.nabu.glue.api;
 
+import java.util.List;
+
 public interface ParameterDescription {
 	public String getName();
 	public String getType();
 	public String getDescription();
+	public List<?> getEnumerations();
 }
--- src/main/java/be/nabu/glue/impl/SimpleParameterDescription.java
+++ src/main/java/be/nabu/glue/impl/SimpleParameterDescription.java
@@ -1,15 +1,21 @@
 package be.nabu.glue.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import be.nabu.glue.api.ParameterDescription;
 
 public class SimpleParameterDescription implements ParameterDescription {
 
 	private String name, description, type;
 	
-	public SimpleParameterDescription(String name, String description, String type) {
+	private List<?> enumerations;
+	
+	public SimpleParameterDescription(String name, String description, String type, Object...enumerations) {
 		this.name = name;
 		this.description = description;
 		this.type = type;
+		this.enumerations = Arrays.asList(enumerations);
 	}
 
 	@Override
@@ -26,5 +32,10 @@ public class SimpleParameterDescription implements ParameterDescription {
 	@Override
 	public String getDescription() {
 		return description;
+	}
+
+	@Override
+	public List<?> getEnumerations() {
+		return enumerations;
 	}
 }
