3f2b7daf9d3978482e0dfaf573c254391d7819ac
alex <alex@nabu.be>
2016-02-07 13:15:48 +0100

    Fixed nested {} inside substitutor

--- src/main/java/be/nabu/glue/impl/ImperativeSubstitutor.java
+++ src/main/java/be/nabu/glue/impl/ImperativeSubstitutor.java
@@ -18,17 +18,35 @@ public class ImperativeSubstitutor implements StringSubstituter {
 		}
 		this.identifier = identifier;
 		this.methodSignature = methodSignature;
-		this.pattern = Pattern.compile("(?<!\\\\)" + Pattern.quote(identifier) + "\\{([^}]+)\\}");
+		this.pattern = Pattern.compile("(?<!\\\\)(" + Pattern.quote(identifier) + "\\{)");
 	}
 	
 	@Override
 	public String substitute(String value, ExecutionContext context, boolean allowNull) {
 		Matcher matcher = pattern.matcher(value);
+		String target = value;
 		while (matcher.find()) {
-			String query = matcher.group().replaceAll(pattern.pattern(), "$1");
-			value = value.replaceAll(Pattern.quote(matcher.group()), "\\${" + Matcher.quoteReplacement(methodSignature.replace("${value}", query)) + "}");
+			int depth = 0;
+			String query = null;
+			// we start after the capturing group
+			int contentStart = matcher.start() + matcher.group(1).length();
+			for (int i = contentStart; i < value.length(); i++) {
+				if (value.charAt(i) == '{') {
+					depth++;
+				}
+				else if (value.charAt(i) == '}') {
+					if (depth == 0) {
+						query = value.substring(contentStart, i);
+						break;
+					}
+					else {
+						depth--;
+					}
+				}
+			}
+			target = target.replaceAll(Pattern.quote(matcher.group() + query + "}"), "\\${" + Matcher.quoteReplacement(methodSignature.replace("${value}", query)) + "}");
 		}
-		return value;
+		return target;
 	}
 
 	public String getIdentifier() {
